384e34b186cbcfc68ad1b1b96af0a97adeb98e5a
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2026-01-26 15:18:22 +0530

    Change default instance type to c7i-flex.large

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -53,7 +53,7 @@ variable "node_groups" {
   }))
   default = {
     general = {
-      instance_types = ["t3.medium"]
+      instance_types = ["c7i-flex.large"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
         desired_size = 2
