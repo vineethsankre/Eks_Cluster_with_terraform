6601b75bf5e85437795c714bcd9e7a531479d1b1
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2026-01-26 14:00:02 +0530

    Rename DynamoDB table for EKS state locking

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "mc-eks-state-lock"
+  name         = "todo-eks-state-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
