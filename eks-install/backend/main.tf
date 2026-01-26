e02dd3c3c073e6be5e7fec4cbaea519532b2cb87
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2026-01-26 14:11:09 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "mindcircuit-eks-bucket"
+  bucket = "todoapp-mcproject-s3-bucket"
 
   lifecycle {
     prevent_destroy = false
